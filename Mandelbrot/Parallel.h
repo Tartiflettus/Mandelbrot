e6f741cbef86dae992465a3d9b9724bfbc33d491
Creusel <victor.creusel@gmail.com>
2017-06-11 16:35:04 +0200

    Modif de ParallelFor pour qu'un séquence s'effectue dans le thread courant

--- Mandelbrot/Parallel.h
+++ Mandelbrot/Parallel.h
@@ -7,7 +7,7 @@
 /*
 Effectue la boucle en parallélisant en fonction du hardware disponible
 T : type de la fonction à appeler, renvoyant un std::future
-func : fonction à appeler, prenant en paramètre l'indice actuel
+func : fonction à appeler, prenant en paramètre l'indice actuel de type compatible int
 */
 template <typename T>
 void parallelFor(int from, int to, T func) {
@@ -18,14 +18,24 @@ void parallelFor(int from, int to, T func) {
 	std::vector<std::future<void>> results;
 	results.reserve(nbProc);
 
-	for (int i = 0; i <= int(nbProc); ++i) {
+	int i = 0;
+	for (; i <= int(nbProc)-1; ++i) { // -1 car dernière itération dans le tread courant
 		const int localFrom = from + i*unit;
 		const int localTo = localFrom + unit > to ? to : localFrom + unit; //dernière itération
 		results.emplace_back(std::async(std::launch::async, [=]{
+			std::cout << i << std::endl;
 			for (int j = localFrom; j < localTo; ++j) {
 				func(j);
 			}
 		}));
+	}
+	if (i > 0) { //dernière itération dans le thread courant
+		const int localFrom = from + i*unit;
+		const int localTo = localFrom + unit > to ? to : localFrom + unit; //dernière itération
+		std::cout << i << std::endl;
+		for (int j = localFrom; j < localTo; ++j) {
+			func(j);
+		}
 	}
 	for (auto& elem : results) {
 		elem.wait();
